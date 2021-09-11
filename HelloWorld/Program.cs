068b76ec2b082d300829bfe1d7dcd24574c41a22
Tom <lordbless77@hotmail.com>
2021-09-11 14:47:25 -0600

    Remommiting previou commit for practice / exercise (Added some carriage returns after hello message)

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
       static void Main(string[] args)
       {
          Console.WriteLine(" Hello from Git repository #1");
+         Console.WriteLine("\n\n\n\n");
       }
    }
 }
